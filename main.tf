550ddc3f27ffa2772a10ff6274ecdb760300cf81
kiran1616 <kirankumar7163@gmail.com>
2023-04-12 23:23:38 +0530

    updated script

---
+++ main.tf
@@ -0,0 +1,35 @@
+#creating subnetgroup for DB
+resource "aws_docdb_subnet_group" "default" {
+  name       = "${var.env}-docdb-subnet-groups"
+  subnet_ids = var.subnet_ids
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-docdb-subnet-groups" }
+  )
+}
+
+# creating security group for Rabbitmq
+resource "aws_security_group" "rabbitmq" {
+  name        = "${var.env}-rabbitmq-security-group"
+  description = "${var.env}-rabbitmq-security-group"
+  vpc_id      = var.vpc_id
+  ingress {
+    description = "rabbitmq"
+    from_port   = 5672
+    to_port     = 5672
+    protocol    = "tcp"
+    cidr_blocks = var.allow_cidr
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-rabbitmq-security-group" }
+  )
+}
+
